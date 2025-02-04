ff1939d68b0e2e8cd89030e6d238f1d94ebd3396
Rakesh Kumar <rakesh@incubyte.co>
2025-02-04 20:07:47 +0530

    feat: handle case when custom delimiter is passed

--- src/string-calculator.ts
+++ src/string-calculator.ts
@@ -3,7 +3,15 @@ export class StringCalculator {
     if (numbers === "") {
       return 0;
     }
-    let parsedNumbers = numbers.split(/[,\n]/).map(Number);
+    let regex = new RegExp(`[,\n]`);
+    if (numbers.startsWith("//")) {
+      let [delimiter, numbersString] = numbers.split("\n");
+      delimiter = delimiter.slice(2);
+      regex = new RegExp(`[${delimiter}]`);
+      numbers = numbersString;
+    }
+
+    let parsedNumbers = numbers.split(regex).map(Number);
     return parsedNumbers.reduce((acc, curr) => acc + curr, 0);
   }
 }
