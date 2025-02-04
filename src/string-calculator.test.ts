97b2a46c39ffb4c55c352043972813c5fdf391e8
Rakesh Kumar <rakesh@incubyte.co>
2025-02-04 19:55:02 +0530

    refactor: add beforeEach for test suite

--- src/string-calculator.test.ts
+++ src/string-calculator.test.ts
@@ -1,13 +1,16 @@
 import { StringCalculator } from "./string-calculator";
 
 describe("StringCalculator", () => {
+  let calculator: StringCalculator;
+  beforeEach(() => {
+    calculator = new StringCalculator();
+  });
+
   it("should return 0 in case of empty string", () => {
-    let calculator = new StringCalculator();
     expect(calculator.add("")).toBe(0);
   });
 
   it("should return the number if single number is passed", () => {
-    let calculator = new StringCalculator();
     expect(calculator.add("4")).toBe(4);
   });
 });
